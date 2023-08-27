2c7941a011aa379327b58b9bf116a8367eb1b22d
coder-Ace77 <adil7k777@gmail.com>
2023-08-27 15:11:12 +0530

    Colors update

--- play.sh
+++ play.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-x=$1
-if [ "$x" == "l" ]; then
+if [ "$1" == "l" ]; then
     while [ true ]; do
     for i in ~/Music/yt-dlp/*; do
-        echo "Looping mode"
-        echo "Playing " $i "USe h for keys and Ctrl for exit"
+        echo -e "\e[1;33m Looping mode"
+        echo -e "\e[0;m Playing \e[1;32m" $i " \e[0;m Use h for keys and Ctrl+q for exit"
         mpg123 -q "$i"
         echo "Use Ctrl+q to exit."
-        sleep 5
+        sleep 1
         clear
     done
     done
@@ -17,19 +16,27 @@ fi
 
 while [ true ]; do
 count=0
+state=0
 for i in ~/Music/yt-dlp/*; do
     count=$[$count + 1]
-    echo $count ")" $i
+    if [ "$state" == "0" ]; then
+        echo -e "\e[1;35m $count." $i
+        state=1
+    else    
+        echo -e "\e[0;m $count." $i
+        state=0
+    fi
+    
 done
-echo "Enter the number of the song you want to play."
+echo -e "\e[1;32mSelect song number"
 read number
 count=0
 clear
 for i in ~/Music/yt-dlp/*; do
     count=$[$count + 1]
     if [ "$count" -eq "$number" ]; then
-        echo "Playing " $i
-        echo "Use h for keys"
+        echo -e "\e[0;m Playing \e[1;32m"$i
+        echo -e "\e[0;m Use h for keys"
         mpg123 -q "$i"
     fi
 done
